0ef0d5fc4114026889fae854891add7c58c74364
Demyiscoding <havefunatwork1021@gmail.com>
2023-04-06 12:08:13 +0800

    修改一些bug

--- js/member.js
+++ js/member.js
@@ -27,12 +27,19 @@ $(function(){
     $(".tab1").click(function(){
         $(".login").show();
         $(".signup").hide();
+        
+
     });
     $(".tab2").click(function(){
         $(".login").hide();
         $(".signup").show();
     });
+ // 模擬點擊 ".tab1"，使登入頁面為預設畫面
+    $(".tab1").click()
+    // $("tab1").addClsss("tab>button:hover");
 });
+
+
 
 // $(function(){
 //     $(".tab2").click(function(){
